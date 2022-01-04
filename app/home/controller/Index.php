5c0d9dafdf58387d7ef815a9ba2df69e94a7c0be
Hor1zon7 <1700892756@qq.com>
2022-01-04 23:50:25 +0800

    新建了主页

--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -1,12 +1,20 @@
 <?php
-declare (strict_types = 1);
+declare (strict_types=1);
 
 namespace app\home\controller;
+
+use app\home\model\User;
 
 class Index
 {
     public function index()
     {
         return view('index');
+    }
+
+    public function home()
+    {
+        $userData=User::select();
+        return  view('home',compact('userData'));
     }
 }
